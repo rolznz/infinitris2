25403bf20fc70f4f811ab62aa49677a182cebf30
Roland Bewick <roland.bewick@gmail.com>
2021-03-24 17:53:34 +0700

    Update privacy policy

--- app/src/components/PrivacyPolicyPage.tsx
+++ app/src/components/PrivacyPolicyPage.tsx
@@ -17,11 +17,20 @@ export function PrivacyPolicyPage() {
         email address under your user account and may use this information to
         send you emails related to Infinitris Online. Any emails sent may
         contain a tracking beacon to determine whether or not the email has been
-        opened. We do not intend to share your private information, but there is
-        always a chance that we could be hacked or legally asked to surrender
-        this information. Use at your own risk. Your progress will be saved in
-        your online account. If you wish to delete your account, please create a
-        ticket on our github.
+        opened.
+      </p>
+      <p>
+        Your Infinitris Online user profile (nickname and any other information
+        you provide by interacting with the service) may be shown on the
+        Infinitris Online scoreboard, lobby or other pages.
+      </p>
+      <p>
+        Infinitris Online is a free game that does not intend to invade your
+        privacy in any way. We do not intend to share your private information
+        (e.g. email address), but there is always a chance that we could be
+        hacked or legally asked to surrender this information. Use at your own
+        risk. Your progress will be saved in your online account. If you wish to
+        delete your account, please create a ticket on our github.
       </p>
 
       <p>
@@ -42,13 +51,6 @@ export function PrivacyPolicyPage() {
         <a href="https://github.com/rolznz/infinitris2">source code</a> for
         further information. If you have any issues, please open a ticket on our
         github.
-      </p>
-
-      <p>
-        Are you reading this? Infinitris Online is a free game that does not
-        intend to invade your privacy in any way. If you are concerned about
-        your own privacy, why don't you throw whatever device you're currently
-        using to read this out of the window, and go live in a cave.
       </p>
 
       <p>

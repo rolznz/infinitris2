13862c610cff1db9a75a4beb7a4c04cd32054fe0
Roland Bewick <roland.bewick@gmail.com>
2021-04-11 16:33:36 +0700

    Make storybook fullscreen

--- app/.storybook/preview.js
+++ app/.storybook/preview.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import App from '@/App';
 import { MemoryRouter } from 'react-router-dom';
+import './storybook.css';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  layout: 'fullscreen',
 };
 
 export const decorators = [

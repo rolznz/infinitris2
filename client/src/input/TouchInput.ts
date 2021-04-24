358c37e4552e5c4d2ab3237ab9430a0c3d771acf
Roland Bewick <roland.bewick@gmail.com>
2021-04-24 20:11:29 +0700

    Improve ingame touch input

--- client/src/input/TouchInput.ts
+++ client/src/input/TouchInput.ts
@@ -1,15 +1,20 @@
 import { InputAction } from 'models';
 import { ActionListener } from './Input';
 
-const MOVEMENT_THRESHOLD = 10;
+const TIME_THRESHOLD = 300;
+const NUM_DIVISIONS = 20;
+
 export default class TouchInput {
   private _fireAction: ActionListener;
   private _pointerX: number;
   private _pointerY: number;
+  private _pointerStartTime: number;
   private _pointerStartX: number;
   private _pointerStartY: number;
   private _lastActionX: number;
   private _lastActionY: number;
+  private _movementThreshold: number;
+  private _hasMoved: boolean;
 
   constructor(fireAction: ActionListener) {
     this._fireAction = fireAction;
@@ -19,6 +24,9 @@ export default class TouchInput {
     this._pointerStartY = 0;
     this._lastActionX = 0;
     this._lastActionY = 0;
+    this._movementThreshold = 0;
+    this._pointerStartTime = 0;
+    this._hasMoved = false;
     document.addEventListener('touchstart', this._onTouchStart);
     document.addEventListener('touchend', this._onTouchEnd);
     document.addEventListener('touchmove', this._onTouchMove);
@@ -31,6 +39,11 @@ export default class TouchInput {
   }
 
   private _onTouchStart = (event: TouchEvent) => {
+    this._hasMoved = false;
+    this._pointerStartTime = Date.now();
+    this._movementThreshold =
+      Math.min(window.innerWidth, window.innerHeight) / NUM_DIVISIONS;
+
     this._pointerX = this._lastActionX = this._pointerStartX =
       event.touches[0].clientX;
     this._pointerY = this._lastActionY = this._pointerStartY =
@@ -38,21 +51,27 @@ export default class TouchInput {
   };
 
   private _onTouchEnd = () => {
-    const pointerChangeX = this._pointerX - this._pointerStartX;
-    const pointerChangeY = this._pointerY - this._pointerStartY;
+    const touchTime = Date.now() - this._pointerStartTime;
+    if (touchTime > TIME_THRESHOLD || this._hasMoved) {
+      return;
+    }
+
+    const totalPointerChangeX = this._pointerX - this._pointerStartX;
+    const totalPointerChangeY = this._pointerY - this._pointerStartY;
     const pointerChangeDistance = Math.sqrt(
-      pointerChangeX * pointerChangeX + pointerChangeY * pointerChangeY
+      totalPointerChangeX * totalPointerChangeX +
+        totalPointerChangeY * totalPointerChangeY
     );
 
-    if (pointerChangeDistance < MOVEMENT_THRESHOLD) {
+    if (pointerChangeDistance < this._movementThreshold) {
       if (this._pointerX < document.body.clientWidth * 0.5) {
         this._fireAction(InputAction.RotateAnticlockwise);
       } else {
         this._fireAction(InputAction.RotateClockwise);
       }
+    } else if (totalPointerChangeY < -this._movementThreshold) {
+      this._fireAction(InputAction.Drop);
     }
-
-    this._fireAction(InputAction.MoveLeft);
   };
 
   private _onTouchMove = (event: TouchEvent) => {
@@ -64,22 +83,25 @@ export default class TouchInput {
     while (true) {
       const pointerChangeX = this._pointerX - this._lastActionX;
       const pointerChangeY = this._pointerY - this._lastActionY;
-      let continueMoving = false;
-      if (pointerChangeX > MOVEMENT_THRESHOLD) {
-        this._lastActionX += MOVEMENT_THRESHOLD;
-        this._fireAction(InputAction.MoveRight);
-        continueMoving = true;
-      } else if (pointerChangeX < -MOVEMENT_THRESHOLD) {
-        this._lastActionX -= MOVEMENT_THRESHOLD;
-        this._fireAction(InputAction.MoveLeft);
-        continueMoving = true;
-      }
-      if (pointerChangeY > MOVEMENT_THRESHOLD) {
-        this._lastActionY += MOVEMENT_THRESHOLD;
-        this._fireAction(InputAction.MoveDown);
-        continueMoving = true;
-      } else if (pointerChangeY < -MOVEMENT_THRESHOLD) {
-        this._fireAction(InputAction.Drop);
+      if (Math.abs(pointerChangeX) > Math.abs(pointerChangeY)) {
+        if (pointerChangeX > this._movementThreshold) {
+          this._lastActionX += this._movementThreshold;
+          this._lastActionY = this._pointerY;
+          this._hasMoved = true;
+          this._fireAction(InputAction.MoveRight);
+        } else if (pointerChangeX < -this._movementThreshold) {
+          this._lastActionX -= this._movementThreshold;
+          this._lastActionY = this._pointerY;
+          this._hasMoved = true;
+          this._fireAction(InputAction.MoveLeft);
+        }
+      } else {
+        if (pointerChangeY > this._movementThreshold) {
+          this._lastActionX = this._pointerX;
+          this._lastActionY += this._movementThreshold;
+          this._hasMoved = true;
+          this._fireAction(InputAction.MoveDown);
+        }
       }
       break;
     }

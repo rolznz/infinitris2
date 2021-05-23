a5eceecc3d5a0cf86b0bd9865400adbf41639ea6
Roland Bewick <roland.bewick@gmail.com>
2021-05-23 15:49:29 +0700

    Fix user firestore rules and tests

--- functions/src/__tests__/users.test.ts
+++ functions/src/__tests__/users.test.ts
@@ -1,4 +1,9 @@
-import { getUserPath, IUser, usersPath } from 'infinitris2-models';
+import {
+  DEFAULT_KEYBOARD_CONTROLS,
+  getUserPath,
+  IUser,
+  usersPath,
+} from 'infinitris2-models';
 import { setup, teardown } from './helpers/setup';
 import './helpers/extensions';
 
@@ -8,6 +13,12 @@ const userId2 = 'userId2';
 const validUserObject: Partial<IUser> = {
   nickname: 'Bob',
   email: 'bob@gmail.com',
+  referredByAffiliateId: '1234',
+  controls: DEFAULT_KEYBOARD_CONTROLS,
+  hasSeenAllSet: false,
+  hasSeenWelcome: true,
+  preferredInputMethod: 'keyboard',
+  locale: 'EN',
 };
 
 describe('Users Rules', () => {
@@ -76,23 +87,103 @@ describe('Users Rules', () => {
   });
 
   test('should not allow setting properties outside allow list', async () => {
-    const db = await setup({ uid: userId1 });
-
     const thisUserPath = getUserPath(userId1);
+    const db = await setup(
+      { uid: userId1 },
+      {
+        [thisUserPath]: validUserObject,
+      }
+    );
 
-    // invalid
+    // ensure can set a valid property to validate below tests are working as expected
+    await expect(
+      db.doc(thisUserPath).set(
+        {
+          email: 'valid@email.com',
+        },
+        { merge: true }
+      )
+    ).toAllow();
+
+    // invalid property
     await expect(
       db.doc(thisUserPath).set({
         credits: 5,
       })
     ).toDeny();
+  });
 
-    // valid
+  test('should not allow setting properties of incorrect type', async () => {
+    const thisUserPath = getUserPath(userId1);
+    const db = await setup(
+      { uid: userId1 },
+      {
+        [thisUserPath]: validUserObject,
+      }
+    );
+
+    // ensure can set a valid property to validate below tests are working as expected
+    await expect(
+      db.doc(thisUserPath).set(
+        {
+          email: 'valid@email.com',
+        },
+        { merge: true }
+      )
+    ).toAllow();
+
+    // invalid property types
     await expect(
       db.doc(thisUserPath).set({
-        email: 'valid@email.com',
+        nickname: 5,
       })
-    ).toAllow();
+    ).toDeny();
+    await expect(
+      db.doc(thisUserPath).set({
+        referredByAffiliateId: 5,
+      })
+    ).toDeny();
+    await expect(
+      db.doc(thisUserPath).set({
+        controls: 5,
+      })
+    ).toDeny();
+    await expect(
+      db.doc(thisUserPath).set({
+        hasSeenAllSet: 5,
+      })
+    ).toDeny();
+    await expect(
+      db.doc(thisUserPath).set({
+        hasSeenWelcome: 5,
+      })
+    ).toDeny();
+    await expect(
+      db.doc(thisUserPath).set({
+        preferredInputMethod: 5,
+      })
+    ).toDeny();
+    await expect(
+      db.doc(thisUserPath).set({
+        locale: 5,
+      })
+    ).toDeny();
+
+    // controls: unsupported control
+    await expect(
+      db.doc(thisUserPath).set({
+        controls: {
+          nonExistentControl: '1',
+        },
+      })
+    ).toDeny();
+    await expect(
+      db.doc(thisUserPath).set({
+        controls: {
+          preferredInputMethod: 'nonexistent',
+        },
+      })
+    ).toDeny();
   });
 
   test('should not be able to delete user', async () => {
@@ -104,5 +195,59 @@ describe('Users Rules', () => {
       }
     );
     await expect(db.doc(thisUserPath).delete()).toDeny();
+  });
+
+  test('should not be able to remove required properties', async () => {
+    const thisUserPath = getUserPath(userId1);
+    const db = await setup(
+      { uid: userId1 },
+      {
+        [thisUserPath]: validUserObject,
+      }
+    );
+    await expect(db.doc(thisUserPath).set({})).toDeny();
+    await expect(
+      db.doc(thisUserPath).set(
+        {
+          email: '',
+        },
+        { merge: true }
+      )
+    ).toDeny();
+    await expect(
+      db.doc(thisUserPath).set(
+        {
+          nickname: '',
+        },
+        { merge: true }
+      )
+    ).toDeny();
+
+    await expect(db.doc(thisUserPath).set({}, { merge: true })).toAllow();
+  });
+
+  test('properties should meet requirements', async () => {
+    const thisUserPath = getUserPath(userId1);
+    const db = await setup(
+      { uid: userId1 },
+      {
+        [thisUserPath]: validUserObject,
+      }
+    );
+    await expect(
+      db.doc(thisUserPath).set({ email: 'a'.repeat(361) }, { merge: true })
+    ).toDeny();
+    await expect(
+      db.doc(thisUserPath).set({ email: 'aa' }, { merge: true })
+    ).toDeny(); // invalid email format
+    await expect(
+      db.doc(thisUserPath).set({ nickname: 'a' }, { merge: true })
+    ).toDeny(); // must be at least 2 chars
+    await expect(
+      db.doc(thisUserPath).set({ nickname: '**' }, { merge: true })
+    ).toDeny(); // invalid chars not allowed
+    await expect(
+      db.doc(thisUserPath).set({ nickname: 'a'.repeat(16) }, { merge: true })
+    ).toDeny();
   });
 });
--- models/src/IUser.ts
+++ models/src/IUser.ts
@@ -4,14 +4,14 @@ import { ChallengeStatus } from './ChallengeStatus';
 import IEntity from './IEntity';
 
 export default interface IUser extends IEntity {
-  readonly challengeAttempts: { [challengeId: string]: ChallengeStatus[] };
   readonly hasSeenWelcome: boolean;
   readonly hasSeenAllSet: boolean;
   readonly nickname: string;
   readonly email: string;
   readonly locale: string;
   readonly preferredInputMethod?: InputMethod;
-  readonly completedChallengeIds: string[];
+  //readonly challengeAttempts: { [challengeId: string]: ChallengeStatus[] };
+  //readonly completedChallengeIds: string[];
   readonly controls: ControlSettings;
   readonly credits: number;
   readonly networkImpact: number;

845e4e9c33df4b5a3146ca964a8daa1346dcf4ec
Roland Bewick <roland.bewick@gmail.com>
2021-07-18 15:37:26 +0700

    Add conversion rules tests

--- functions/src/__tests__/conversionRules.test.ts
+++ functions/src/__tests__/conversionRules.test.ts
@@ -1,151 +1,126 @@
-/*import { setup, teardown } from './helpers/setup';
+import { setup, teardown } from './helpers/setup';
 import './helpers/extensions';
 import dummyData from './helpers/dummyData';
 
-describe('Referred By Affiliate Rules', () => {
+import { getConversionPath, IUser } from 'infinitris2-models';
+
+describe('Conversion Rules', () => {
   afterEach(async () => {
     await teardown();
   });
 
-  test('should allow creating a referred by affiliate request for self', async () => {
+  test('should allow creating a conversion for an affiliate', async () => {
     const { db } = await setup(
       { uid: dummyData.userId1 },
       {
-        [dummyData.user1Path]: dummyData.existingUser,
         [dummyData.affiliate1Path]: dummyData.affiliate1,
-      }
-    );
-
-    await expect(
-      db
-        .doc(dummyData.conversion1Path)
-        .set(dummyData.referredByAffiliateRequest)
-    ).toAllow();
-  });
-
-  test('should deny creating a referred by affiliate request with invalid parameters', async () => {
-    const { db } = await setup(
-      { uid: dummyData.userId1 },
-      {
         [dummyData.user1Path]: dummyData.existingUser,
       }
     );
 
     await expect(
-      db.doc(dummyData.conversion1Path).set({
-        ...dummyData.referredByAffiliateRequest,
-        nonExistentProperty: '5',
-      })
-    ).toDeny();
-
-    await expect(
-      db.doc(dummyData.conversion1Path).set({
-        ...dummyData.referredByAffiliateRequest,
-        referredByAffiliateId: 123,
-      })
-    ).toDeny();
-    await expect(
-      db.doc(dummyData.conversion1Path).set({
-        ...dummyData.referredByAffiliateRequest,
-        referredByAffiliateId: '',
-      })
-    ).toDeny();
-  });
-
-  // FIXME: all these need to be reviewed
-
-  test('should deny creating a conversion when logged out', async () => {
-    const { db } = await setup(undefined, {
-      [dummyData.user1Path]: dummyData.existingUser,
-    });
-
-    await expect(
-      db
-        .doc(dummyData.conversion1Path)
-        .set(dummyData.referredByAffiliateRequest)
-    ).toDeny();
+      db.doc(dummyData.conversion1Path).set(dummyData.conversion1)
+    ).toAllow();
   });
 
   test('should deny creating a conversion for another user', async () => {
     const { db } = await setup(
       { uid: dummyData.userId2 },
       {
+        [dummyData.affiliate1Path]: dummyData.affiliate1,
         [dummyData.user1Path]: dummyData.existingUser,
       }
     );
 
     await expect(
-      db
-        .doc(dummyData.conversion1Path)
-        .set(dummyData.referredByAffiliateRequest)
+      db.doc(dummyData.conversion1Path).set(dummyData.conversion1)
     ).toDeny();
   });
 
-  test('should allow creating a conversion for self', async () => {
+  test('should deny creating a conversion for own affiliate', async () => {
+    const { db } = await setup(
+      { uid: dummyData.userId2 },
+      {
+        [dummyData.affiliate1Path]: dummyData.affiliate1,
+        [dummyData.user2Path]: dummyData.existingUser,
+      }
+    );
+
+    const conversionPath = getConversionPath(
+      dummyData.affiliateId1,
+      dummyData.userId2
+    );
+
+    await expect(db.doc(conversionPath).set(dummyData.conversion1)).toDeny();
+  });
+
+  test('should deny creating a conversion for non-existent affiliate', async () => {
     const { db } = await setup(
       { uid: dummyData.userId1 },
       {
-        [dummyData.user1Path]: dummyData.existingUser,
         [dummyData.affiliate1Path]: dummyData.affiliate1,
+        [dummyData.user1Path]: dummyData.existingUser,
+      }
+    );
+
+    const conversionPath = getConversionPath('nonexistent', dummyData.userId1);
+
+    await expect(db.doc(conversionPath).set(dummyData.conversion1)).toDeny();
+  });
+
+  test('should deny creating a conversion for an already converted user', async () => {
+    const user1: IUser = {
+      ...dummyData.existingUser,
+      readOnly: {
+        ...dummyData.existingUser.readOnly,
+        referredByAffiliateId: dummyData.affiliateId1,
+      },
+    };
+
+    const { db } = await setup(
+      { uid: dummyData.userId1 },
+      {
+        [dummyData.affiliate1Path]: dummyData.affiliate1,
+        [dummyData.user1Path]: user1,
       }
     );
 
     await expect(
-      db
-        .doc(dummyData.conversion1Path)
-        .set(dummyData.referredByAffiliateRequest)
-    ).toAllow();
+      db.doc(dummyData.conversion1Path).set(dummyData.conversion1)
+    ).toDeny();
   });
 
-  test('should not allow creating a conversion with created property set to true', async () => {
+  test('should deny creating a conversion with invalid parameters', async () => {
     const { db } = await setup(
       { uid: dummyData.userId1 },
       {
-        [dummyData.user1Path]: dummyData.existingUser,
         [dummyData.affiliate1Path]: dummyData.affiliate1,
+        [dummyData.user1Path]: dummyData.existingUser,
       }
     );
 
     await expect(
       db.doc(dummyData.conversion1Path).set({
-        ...dummyData.referredByAffiliateRequest,
+        ...dummyData.conversion1,
+        nonExistentParameter: 1,
+      })
+    ).toDeny();
+  });
+
+  test('should deny creating a conversion with created property set to true', async () => {
+    const { db } = await setup(
+      { uid: dummyData.userId1 },
+      {
+        [dummyData.affiliate1Path]: dummyData.affiliate1,
+        [dummyData.user1Path]: dummyData.existingUser,
+      }
+    );
+
+    await expect(
+      db.doc(dummyData.conversion1Path).set({
+        ...dummyData.conversion1,
         created: true,
-      } as IUserRequest)
-    ).toDeny();
-  });
-
-  test('should deny updating a conversion', async () => {
-    const { db } = await setup(
-      { uid: dummyData.userId1 },
-      {
-        [dummyData.user1Path]: dummyData.existingUser,
-        [dummyData.affiliate1Path]: dummyData.affiliate1,
-        [dummyData.conversion1Path]: dummyData.referredByAffiliateRequest,
-      }
-    );
-
-    await expect(
-      db
-        .doc(dummyData.conversion1Path)
-        .set(dummyData.referredByAffiliateRequest)
-    ).toDeny();
-  });
-
-  test('should deny creating an unsupported request', async () => {
-    const { db } = await setup(
-      { uid: dummyData.userId1 },
-      {
-        [dummyData.user1Path]: dummyData.existingUser,
-        [dummyData.affiliate1Path]: dummyData.affiliate1,
-      }
-    );
-
-    await expect(
-      db.doc(dummyData.conversion1Path).set({
-        ...dummyData.referredByAffiliateRequest,
-        requestType: 'unknownRequestType',
       })
     ).toDeny();
   });
 });
-*/

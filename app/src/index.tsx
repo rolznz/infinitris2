b923d9db68a1e0dc70a19e327672519978ecdc70
Roland Bewick <roland.bewick@gmail.com>
2022-02-07 12:55:38 +0700

    Remove unused service worker

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import Loader from './components/ui/Loader';
 import PageRouter from './PageRouter';
 import smoothscroll from 'smoothscroll-polyfill';
@@ -19,8 +18,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();

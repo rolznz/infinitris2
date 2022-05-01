25ca1ff04d75588b440e67b33c752e1f3e274fed
Roland Bewick <roland.bewick@gmail.com>
2022-05-01 15:08:05 +0700

    Remove fullstop after login code in email

--- functions/src/utils/sendLoginCode.ts
+++ functions/src/utils/sendLoginCode.ts
@@ -4,7 +4,7 @@ export function sendLoginCode(email: string, code: string) {
   return sendEmail(
     email,
     'Infinitris Login Code',
-    `To login, enter the following code within 10 minutes: <span style="font-size: 2rem;">${code}</span>.` +
+    `To login, enter the following code within 10 minutes: <span style="font-size: 2rem;">${code}</span>` +
       '<br/><br/>If you did not attempt to login to Infinitris, please ignore this email.'
   );
 }

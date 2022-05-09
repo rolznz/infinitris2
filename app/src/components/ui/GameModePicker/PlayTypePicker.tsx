27de41ab9b20cf212a55106e8cb5ad2a26d24c42
Roland Bewick <roland.bewick@gmail.com>
2022-05-09 21:40:40 +0700

    Temporarily disable community challenges link

--- app/src/components/ui/GameModePicker/PlayTypePicker.tsx
+++ app/src/components/ui/GameModePicker/PlayTypePicker.tsx
@@ -88,7 +88,7 @@ export function PlayTypePicker({ paddingTop, display }: GameModePickerProps) {
         link={Routes.lobby}
       />
       <PlayTypeCard
-        link={Routes.challenges}
+        //link={Routes.challenges}
         image={
           isLandscape
             ? isDarkMode

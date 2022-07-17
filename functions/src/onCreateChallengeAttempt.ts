56252cacfe5c6721aca0275ae9dbd83aea6429e7
Roland Bewick <roland.bewick@gmail.com>
2022-07-17 14:55:18 +0700

    Post challenge attempt top placing to external webhook

--- functions/src/onCreateChallengeAttempt.ts
+++ functions/src/onCreateChallengeAttempt.ts
@@ -1,12 +1,15 @@
 import * as functions from 'firebase-functions';
 import {
   getChallengePath,
+  getUserPath,
   IChallenge,
   IChallengeAttempt,
+  IUser,
   objectToDotNotation,
 } from 'infinitris2-models';
 import { getDb, increment } from './utils/firebase';
 import { getDefaultEntityReadOnlyProperties } from './utils/getDefaultEntityReadOnlyProperties';
+import { postSimpleWebhook } from './utils/postSimpleWebhook';
 
 export const onCreateChallengeAttempt = functions.firestore
   .document('challengeAttempts/{challengeAttemptId}')
@@ -41,6 +44,37 @@ export const onCreateChallengeAttempt = functions.firestore
           },
           created: true,
         } as Pick<IChallengeAttempt, 'readOnly' | 'created'>);
+
+      const topAttempts = await getDb()
+        .collection('challengeAttempts')
+        .where('challengeId', '==', challengeAttempt.challengeId)
+        .orderBy('stats.timeTakenMs')
+        .limit(2)
+        .get();
+      if (
+        topAttempts.docs.length < 2 ||
+        (topAttempts.docs[0].id === snapshot.id &&
+          (topAttempts.docs[1].data() as IChallengeAttempt).userId !== userId)
+      ) {
+        const userDocRef = getDb().doc(getUserPath(userId));
+        const userDoc = await userDocRef.get();
+        const userData = userDoc.data() as IUser;
+        const challengeDoc = await challengeRef.get();
+        const challengeData = challengeDoc.data() as IChallenge;
+        postSimpleWebhook(
+          `${
+            userData.readOnly.nickname || 'Unknown user'
+          } took first place on ${
+            challengeData.title || 'Unnamed challenge'
+          } with a time of ${(
+            challengeAttempt.stats.timeTakenMs / 1000
+          ).toFixed(
+            2
+          )}s\n\nView their replay or try for a faster time: https://infinitris.net/challenges/${
+            challengeAttempt.challengeId
+          }`
+        );
+      }
     } catch (error) {
       console.error(error);
     }

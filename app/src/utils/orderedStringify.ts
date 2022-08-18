4fb7d7772ce07f53032d25d6ca257cff590ca372
Roland Bewick <roland.bewick@gmail.com>
2022-08-18 12:54:22 +0700

    Fix orderedStringify not correctly stringifying nested object

--- app/src/utils/orderedStringify.ts
+++ app/src/utils/orderedStringify.ts
@@ -1,6 +1,14 @@
-// from https://stackoverflow.com/a/53593328/4562693
+// from https://stackoverflow.com/a/43636793/4562693
+const replacer = (key: string, value: any) =>
+  value instanceof Object && !(value instanceof Array)
+    ? Object.keys(value)
+        .sort()
+        .reduce((sorted: any, key: string) => {
+          sorted[key] = value[key];
+          return sorted;
+        }, {})
+    : value;
+
 export function orderedStringify(obj: object) {
-  const allKeys = new Set<string>();
-  JSON.stringify(obj, (key, _value) => allKeys.add(key));
-  return JSON.stringify(obj, Array.from(allKeys).sort(), undefined);
+  return JSON.stringify(obj, replacer);
 }

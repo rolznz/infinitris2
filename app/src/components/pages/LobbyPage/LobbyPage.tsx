3b034ae473286d77c7563f3acb429c78dc0c0e89
Roland Bewick <roland.bewick@gmail.com>
2022-09-16 11:19:48 +0700

    Add play tutorial toast to lobby page

--- app/src/components/pages/LobbyPage/LobbyPage.tsx
+++ app/src/components/pages/LobbyPage/LobbyPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 import { ReactComponent as RefreshIcon } from '@/icons/refresh.svg';
 import { RoomCarousel } from '@/components/ui/RoomCarousel/RoomCarousel';
 import { ReactComponent as InfoIcon } from '@/icons/i.svg';
@@ -10,6 +10,13 @@ import IconButton from '@mui/material/IconButton';
 import { useLobbyServers } from '@/components/hooks/useLobbyServers';
 import useLatest from 'react-use/lib/useLatest';
 import SvgIcon from '@mui/material/SvgIcon/SvgIcon';
+import useIncompleteChallenges from '@/components/hooks/useIncompleteChallenges';
+import { useSnackbar } from 'notistack';
+import { appName } from '@/utils/constants';
+import { ReactComponent as PlayIcon } from '@/icons/play.svg';
+import { ReactComponent as CloseIcon } from '@/icons/x.svg';
+import FlexBox from '@/components/ui/FlexBox';
+import useLoaderStore from '@/state/LoaderStore';
 
 let cachedRoomId: string | undefined;
 
@@ -19,6 +26,56 @@ export default function LobbyPage() {
   const { roomServerPairs, refresh } = useLobbyServers(cachedRoomId);
   const latestCurrentSlide = useLatest(currentSlide);
   const latestRoomServerPairs = useLatest(roomServerPairs);
+  const incompleteChallengesCount =
+    useIncompleteChallenges('grass').incompleteChallenges?.length || 0;
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+  const intl = useIntl();
+  const loaderHasFinished = useLoaderStore((store) => store.hasFinished);
+
+  // TODO: consider moving this code to a shared hook and show on other pages too
+  React.useEffect(() => {
+    if (incompleteChallengesCount > 0 && loaderHasFinished) {
+      enqueueSnackbar(
+        intl.formatMessage(
+          {
+            defaultMessage: `New to {appName}? play the tutorial!`,
+            description: 'New player tutorial recommendation message',
+          },
+          { appName }
+        ),
+        {
+          persist: true,
+          variant: 'info',
+          action: (
+            <FlexBox flexDirection="row" gap={1}>
+              <IconButton
+                onClick={() => {
+                  closeSnackbar();
+                  history.push(Routes.storyMode);
+                }}
+              >
+                <SvgIcon color="primary" fontSize="large">
+                  <PlayIcon />
+                </SvgIcon>
+              </IconButton>
+              <IconButton onClick={() => closeSnackbar()}>
+                <SvgIcon color="primary" fontSize="small">
+                  <CloseIcon />
+                </SvgIcon>
+              </IconButton>
+            </FlexBox>
+          ),
+        }
+      );
+    }
+  }, [
+    incompleteChallengesCount,
+    enqueueSnackbar,
+    intl,
+    closeSnackbar,
+    history,
+    loaderHasFinished,
+  ]);
 
   const slides: RoomCarouselSlideProps[] | undefined = React.useMemo(
     () =>

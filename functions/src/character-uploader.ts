c9a9d1f812626237ca74debd4383a63d825a9d69
Roland Bewick <roland.bewick@gmail.com>
2022-09-19 14:09:19 +0700

    Upload svg patterns as part of character uploader process

--- functions/src/character-uploader.ts
+++ functions/src/character-uploader.ts
@@ -4,6 +4,7 @@ import * as functions from 'firebase-functions';
 
 const inputDir = '../character-generator/out';
 const patternsDir = `${inputDir}/patterns`;
+const svgPatternsDir = `../character-generator/assets`;
 const definitionsDirectory = `${inputDir}/definitions`;
 const bucketName = functions.config().webhooks.images_bucket;
 if (!bucketName) {
@@ -20,60 +21,82 @@ const patternFilenames = readdirSync(patternsDir).filter((filename) =>
   filename.endsWith('.png')
 );
 
-const characterFilenames = readdirSync(definitionsDirectory).filter(
-  (filename) => filename.endsWith('.json')
-);
-console.log('Uploading patterns');
-(async () => {
-  for (const filename of patternFilenames) {
-    await uploadFile(`${patternsDir}/${filename}`, `patterns/${filename}`);
-    process.stdout.write('.');
-  }
-})();
-console.log('Uploading ' + characterFilenames.length + ' characters');
-let index = 0;
-(async () => {
-  for (const filename of characterFilenames) {
-    // console.log(filename);
-    const promises: Promise<any>[] = [];
-    const contents = JSON.parse(
-      readFileSync(`${definitionsDirectory}/${filename}`).toString()
-    );
-    const id = parseInt(filename.substring(0, filename.indexOf('.')));
-
-    // console.log(contents);
-    promises.push(
-      getDb().doc(`characters/${id}`).set(contents, { merge: true })
-    );
-
-    const imageFilename = `${id}.png`;
-    const thumbnailFilename = `${id}_thumbnail.png`;
-    const habitatFilename = `${id}.svg`;
-    promises.push(
-      uploadFile(
-        `${inputDir}/characters/${imageFilename}`,
-        `characters/${imageFilename}`
-      )
-    );
-    promises.push(
-      uploadFile(
-        `${inputDir}/characters/${thumbnailFilename}`,
-        `characters/${thumbnailFilename}`
-      )
-    );
-    promises.push(
-      uploadFile(`${inputDir}/faces/${imageFilename}`, `faces/${imageFilename}`)
-    );
-    promises.push(
-      uploadFile(
-        `${inputDir}/habitats/${habitatFilename}`,
-        `habitats/${habitatFilename}`
-      )
-    );
-    await Promise.all(promises);
-    process.stdout.write('.');
-    if (++index % 100 === 0) {
-      console.log(index + ' / ' + characterFilenames.length);
+if (process.env.UPLOAD_PNG_PATTERNS === 'true') {
+  console.log('Uploading png patterns');
+  (async () => {
+    for (const filename of patternFilenames) {
+      await uploadFile(`${patternsDir}/${filename}`, `patterns/${filename}`);
+      process.stdout.write('.');
     }
-  }
-})();
+  })();
+}
+
+if (process.env.UPLOAD_SVG_PATTERNS === 'true') {
+  const svgPatternFilenames = readdirSync(svgPatternsDir).filter(
+    (filename) => filename.startsWith('pattern_') && filename.endsWith('.svg')
+  );
+  console.log('Uploading svg patterns');
+  (async () => {
+    for (const filename of svgPatternFilenames) {
+      await uploadFile(`${svgPatternsDir}/${filename}`, `patterns/${filename}`);
+      process.stdout.write('.');
+    }
+  })();
+}
+
+if (process.env.UPLOAD_CHARACTERS === 'true') {
+  const characterFilenames = readdirSync(definitionsDirectory).filter(
+    (filename) => filename.endsWith('.json')
+  );
+
+  console.log('Uploading ' + characterFilenames.length + ' characters');
+  let index = 0;
+  (async () => {
+    for (const filename of characterFilenames) {
+      // console.log(filename);
+      const promises: Promise<any>[] = [];
+      const contents = JSON.parse(
+        readFileSync(`${definitionsDirectory}/${filename}`).toString()
+      );
+      const id = parseInt(filename.substring(0, filename.indexOf('.')));
+
+      // console.log(contents);
+      promises.push(
+        getDb().doc(`characters/${id}`).set(contents, { merge: true })
+      );
+
+      const imageFilename = `${id}.png`;
+      const thumbnailFilename = `${id}_thumbnail.png`;
+      const habitatFilename = `${id}.svg`;
+      promises.push(
+        uploadFile(
+          `${inputDir}/characters/${imageFilename}`,
+          `characters/${imageFilename}`
+        )
+      );
+      promises.push(
+        uploadFile(
+          `${inputDir}/characters/${thumbnailFilename}`,
+          `characters/${thumbnailFilename}`
+        )
+      );
+      promises.push(
+        uploadFile(
+          `${inputDir}/faces/${imageFilename}`,
+          `faces/${imageFilename}`
+        )
+      );
+      promises.push(
+        uploadFile(
+          `${inputDir}/habitats/${habitatFilename}`,
+          `habitats/${habitatFilename}`
+        )
+      );
+      await Promise.all(promises);
+      process.stdout.write('.');
+      if (++index % 100 === 0) {
+        console.log(index + ' / ' + characterFilenames.length);
+      }
+    }
+  })();
+}

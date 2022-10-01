7a694f5a065fba40fcbab44dc23a45e326c918b1
Roland Bewick <roland.bewick@gmail.com>
2022-10-01 13:35:30 +0700

    Add satoshi prices to invoices

--- app/src/components/ui/LightningQR.tsx
+++ app/src/components/ui/LightningQR.tsx
@@ -2,12 +2,13 @@ import FlexBox from '@/components/ui/FlexBox';
 import { colors } from '@/theme/theme';
 import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
-import { useIntl } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 import QRCode from 'react-qr-code';
 import { useCopyToClipboard } from 'react-use';
 import { useTheme } from '@mui/material/styles';
 import isMobile from '@/utils/isMobile';
 import { useSnackbar } from 'notistack';
+import * as bolt11 from 'bolt11';
 
 type LightningQRProps = {
   value: string;
@@ -18,6 +19,8 @@ export function LightningQR({ value }: LightningQRProps) {
   const intl = useIntl();
   const theme = useTheme();
   const { enqueueSnackbar } = useSnackbar();
+  const priceInSats =
+    parseInt(bolt11.decode(value).millisatoshis || '0') / 1000;
   return (
     <FlexBox>
       <QRCode
@@ -36,12 +39,22 @@ export function LightningQR({ value }: LightningQRProps) {
           );
         }}
       />
+      <Typography align="center" variant="body1" mt={1}>
+        <FormattedMessage
+          defaultMessage="Price: {priceInSats, plural, =1 {# satoshi} other {# satoshis}}"
+          values={{ priceInSats }}
+        />
+      </Typography>
       {isMobile() && (
         <Typography align="center" variant="caption" mt={1}>
           Tap to copy
         </Typography>
       )}
-      <Typography align="center" variant="caption" mt={1}>
+      <Typography align="center" variant="caption">
+        <Link href="https://www.coinbase.com/converter/sats/usd">
+          check conversion rates
+        </Link>
+        {' • '}
         Payments powered by <Link href="https://lnbits.com/">lnbits</Link>
       </Typography>
     </FlexBox>
